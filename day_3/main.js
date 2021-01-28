3a67ff790bfff9a382ab310babc0bc5a56978f7a
bartoszkruba <bartosz.kruba@gmail.com>
2021-01-28 19:08:56 +0100

    Solution for day 3, part two

--- day_3/main.js
+++ day_3/main.js
@@ -2,14 +2,14 @@ const fs = require('fs')
 
 const inputs = fs.readFileSync('puzzle_inputs.txt').toString().split('\n').map(line => line.split(','))
 
-
-function findSolution1(inputs) {
-    const wire_coordinates = []
+const wireCoordinates = (inputs) => {
+    const wireCoordinates = []
 
     for (input of inputs) {
         position = [0, 0]
-        const coords = new Set()
+        const coords = {}
 
+        let totalSteps = 0
         for (point of input) {
             direction = point[0]
             value = +point.slice(1)
@@ -29,19 +29,34 @@ function findSolution1(inputs) {
                         position[1] -= 1
                         break
                 }
-
-                coords.add(position[0] + "|" + position[1])
+                
+                totalSteps++
+                key = position[0] + "|" + position[1]
+                if (!(key in coords)) coords[key] = totalSteps
             }
         }
 
-        wire_coordinates.push(coords)
+        wireCoordinates.push(coords)
     }
 
-    const closest = [...wire_coordinates[0]].filter(coordinate => wire_coordinates[1].has(coordinate))
-        .map(crossing => Math.abs(+crossing.split('|')[0]) + Math.abs(+crossing.split('|')[1]))
-        .sort((a, b) => a - b)[0]
-
-    return closest
+    return wireCoordinates
 }
 
-console.log('Part One Solution: ' + findSolution1(inputs))
+
+const findSolutions = (inputs) => {
+    const coordinates = wireCoordinates(inputs)
+
+    const crossings = Object.keys(coordinates[0]).filter(coordinate => coordinate in coordinates[1])
+
+    const closest = crossings.map(crossing => Math.abs(+crossing.split('|')[0]) + Math.abs(+crossing.split('|')[1]))
+        .sort((a, b) => a - b)[0]
+
+    const fastests = crossings.map(coord => coordinates[0][coord] + coordinates[1][coord])
+        .sort((a, b) => a - b)[0]
+
+    return {closest, fastests}
+}
+
+const {closest, fastests} = findSolutions(inputs)
+console.log('Part One Solution: ' + closest)
+console.log('Part Two Solution: ' + fastests)
