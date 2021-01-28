8f30c75753a8213ab99c00bee4f1e3267a9664b3
bartoszkruba <bartosz.kruba@gmail.com>
2021-01-28 18:35:58 +0100

    Solution for day 3, part one

--- day_3/main.js
+++ day_3/main.js
@@ -3,43 +3,45 @@ const fs = require('fs')
 const inputs = fs.readFileSync('puzzle_inputs.txt').toString().split('\n').map(line => line.split(','))
 
 
-const wire_coordinates = []
+function findSolution1(inputs) {
+    const wire_coordinates = []
 
-for (input of inputs) {
-    position = [0, 0]
-    const coords = new Set()
+    for (input of inputs) {
+        position = [0, 0]
+        const coords = new Set()
 
-    for (point of input) {
-        direction = point[0]
-        value = +point.slice(1)
+        for (point of input) {
+            direction = point[0]
+            value = +point.slice(1)
 
-        for (let i = 0; i < value; i++) {
-            switch (direction) {
-                case 'R':
-                    position[0] += 1
-                    break
-                case 'L':
-                    position[0] -= 1
-                    break
-                case 'U':
-                    position[1] += 1
-                    break
-                case 'D':
-                    position[1] -= 1
-                    break
+            for (let i = 0; i < value; i++) {
+                switch (direction) {
+                    case 'R':
+                        position[0] += 1
+                        break
+                    case 'L':
+                        position[0] -= 1
+                        break
+                    case 'U':
+                        position[1] += 1
+                        break
+                    case 'D':
+                        position[1] -= 1
+                        break
+                }
+
+                coords.add(position[0] + "|" + position[1])
             }
-
-            coords.add(position[0] + "|" + position[1])
         }
+
+        wire_coordinates.push(coords)
     }
 
-    wire_coordinates.push(coords)
+    const closest = [...wire_coordinates[0]].filter(coordinate => wire_coordinates[1].has(coordinate))
+        .map(crossing => Math.abs(+crossing.split('|')[0]) + Math.abs(+crossing.split('|')[1]))
+        .sort((a, b) => a - b)[0]
+
+    return closest
 }
 
-corssings = new Set()
-
-const lowest = [...wire_coordinates[0]].filter(coordinate => wire_coordinates[1].has(coordinate))
-                        .map(crossing => Math.abs(+crossing.split('|')[0]) + Math.abs(+crossing.split('|')[1]))
-                        .sort()[0]
-
-console.log(lowest)
+console.log('Part One Solution: ' + findSolution1(inputs))
