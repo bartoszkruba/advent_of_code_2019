a93a1ba89cd6ac6de852b67db14066112ddca217
bartoszkruba <bartosz.kruba@gmail.com>
2021-02-02 18:12:33 +0100

    Added solution for day 6, part two

--- day_6/main.js
+++ day_6/main.js
@@ -10,22 +10,36 @@ fs.readFileSync('puzzle_inputs.txt').toString().split('\n').forEach(s => {
     totalPlanets.add(planets[1])
 })
 
-const orbitCount = (orbits, planet, total = 0) => {
-    if (!(planet in orbits)) return total
-    return orbitCount(orbits, orbits[planet], total + 1)
+const orbitList = (orbits, planet, list = []) => {
+    if (!(planet in orbits)) return list
+
+    list.push(orbits[planet])
+    return orbitList(orbits, orbits[planet], list)
 }
 
 const findSolutions = (orbits, totalPlanets) => {
     let total = 0
     for (planet of totalPlanets) {
-        total += orbitCount(orbits, planet)
+        total += orbitList(orbits, planet).length
     }
 
     const solution1 = total
 
-    return {solution1}
+    const myOrbits = orbitList(orbits, 'YOU')
+    const santasOrbits = orbitList(orbits, 'SAN')
+
+    let solution2
+    for (planet of myOrbits) {
+        if (santasOrbits.includes(planet)) {
+            solution2 = myOrbits.indexOf(planet) + santasOrbits.indexOf(planet)
+            break
+        }
+    }
+
+    return { solution1, solution2 }
 }
 
-const {solution1} = findSolutions(orbits, totalPlanets)
+const { solution1, solution2 } = findSolutions(orbits, totalPlanets)
 
 console.log('Part One Solution: ' + solution1)
+console.log('Part Two Solution: ' + solution2)
