b214e25a79db8a276fa58acfa54ef27e5035d28f
bartoszkruba <bartosz.kruba@gmail.com>
2021-02-18 20:03:33 +0100

    Working on solution for day 12, part two (algorithm works on test inputs but its to slow for the real puzzle)

--- day_12/main.js
+++ day_12/main.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 const positions = {}
 const velocities = {}
 
-fs.readFileSync('puzzle_inputs.txt').toString().split('\n').forEach((line, indx) => {
+fs.readFileSync('test_inputs.txt').toString().split('\n').forEach((line, indx) => {
     const position = []
 
     line.replace(/>| |<|=|x|y|z/g, '').split(',').forEach((v, indx) => position[indx] = +v)
@@ -18,34 +18,90 @@ const generatePairs = arr => {
     return pairs
 }
 
-const findSolutions = (positions, velocities) => {
-    const pairs = generatePairs(Object.keys(positions))
+const timeStep = (positions, velocities, pairs) => {
+    for (pair of pairs) {
+        position1 = positions[pair[0]]
+        position2 = positions[pair[1]]
 
-    for (let i = 0; i < 1000; i++) {
-        for (pair of pairs) {
-            position1 = positions[pair[0]]
-            position2 = positions[pair[1]]
+        velocity1 = velocities[pair[0]]
+        velocity2 = velocities[pair[1]]
 
-            velocity1 = velocities[pair[0]]
-            velocity2 = velocities[pair[1]]
-
-            for (let j = 0; j < 3; j++) {
-                if (position1[j] > position2[j]) {
-                    velocity2[j] += 1
-                    velocity1[j] -= 1
-                } else if (position1[j] < position2[j]) {
-                    velocity2[j] -= 1
-                    velocity1[j] += 1
-                }
-            }
-        }
-
-        for (key in positions) {
-            for (let i = 0; i < 3; i++) {
-                positions[key][i] += velocities[key][i]
+        for (let j = 0; j < 3; j++) {
+            if (position1[j] > position2[j]) {
+                velocity2[j] += 1
+                velocity1[j] -= 1
+            } else if (position1[j] < position2[j]) {
+                velocity2[j] -= 1
+                velocity1[j] += 1
             }
         }
     }
+
+    for (key in positions) {
+        for (let i = 0; i < 3; i++) {
+            positions[key][i] += velocities[key][i]
+        }
+    }
+}
+
+const findPatternLength = history => {
+    let length = 1
+
+    while (true) {
+        const h = history.length % length !== 0 ? history.slice(0, -(history.length % length)) : history.map(v => v)
+        const repetitions = h.length / length;
+        const pattern = h.slice(0, length).join('')
+
+        if (pattern.repeat(repetitions) === h.join('')) {
+            return length
+        };
+
+        length++
+    }
+}
+
+const findSolutions = (positions, velocities) => {
+    const pairs = generatePairs(Object.keys(positions))
+
+    let initial = ''
+    for (let key in positions) {
+        for (v of positions[key]) initial += v
+    }
+
+    for (let key in velocities) {
+        for (v of velocities[key]) initial += v
+    }
+
+    let positionsCopy = JSON.parse(JSON.stringify(positions))
+    let velocitiesCopy = JSON.parse(JSON.stringify(velocities))
+
+    const history = []
+    for (let i = 0; i < Object.keys(positions).length * 3; i++) history.push([])
+
+    for (let i = 0; i < 1000; i++) {
+        timeStep(positionsCopy, velocitiesCopy, pairs)
+
+        let i = 0
+        for (let key in positionsCopy)
+            for (let j = 0; j < positions[0].length; j++) history[+key * 3 + j].push(positionsCopy[key][j])
+    }
+
+    let patternLengths = history.map(h => findPatternLength(h))
+    let step = patternLengths.pop()
+    let i = step
+
+    while (patternLengths.length > 0) {
+        patternLengths = patternLengths.filter(l => {
+            if (i % l === 0) {
+                step = i
+                return false
+            }
+            return true
+        })
+        if (patternLengths.length > 0)
+            i += step
+    }
+    console.log(i)
 
     let total = 0
 
@@ -53,8 +109,8 @@ const findSolutions = (positions, velocities) => {
         let potential = 0
         let kinetic = 0
         for (let i = 0; i < 3; i++) {
-            potential += Math.abs(positions[key][i])
-            kinetic += Math.abs(velocities[key][i])
+            potential += Math.abs(positionsCopy[key][i])
+            kinetic += Math.abs(velocitiesCopy[key][i])
 
         }
 
@@ -63,9 +119,10 @@ const findSolutions = (positions, velocities) => {
 
     const solution1 = total
 
-    return {solution1}
+
+    return { solution1 }
 }
 
-const {solution1} = findSolutions(positions, velocities)
+const { solution1 } = findSolutions(positions, velocities)
 console.log('Part One Solution: ' + solution1)
 
